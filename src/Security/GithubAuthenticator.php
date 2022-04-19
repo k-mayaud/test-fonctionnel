544edbea8787fd936664c50891cbaa15ee669f3c
Kévin Mayaud <mayaud@ext.lemonde.fr>
2022-04-19 15:47:57 +0200

    Update for functional test course.

--- src/Security/GithubAuthenticator.php
+++ src/Security/GithubAuthenticator.php
@@ -2,78 +2,124 @@
 
 namespace App\Security;
 
-use GuzzleHttp\Client;
-use Symfony\Bundle\FrameworkBundle\Routing\Router;
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use KnpU\OAuth2ClientBundle\Security\Authenticator\OAuth2Authenticator;
+use League\OAuth2\Client\Provider\GithubResourceOwner;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken;
-use Symfony\Component\Security\Core\Exception\BadCredentialsException;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 
-class GithubAuthenticator implements AuthenticationFailureHandlerInterface
+class GithubAuthenticator extends OAuth2Authenticator
 {
-    private Client $client;
-    private int $clientId;
-    private string $clientSecret;
-    private Router $router;
+    private ClientRegistry $clientRegistry;
+    private RouterInterface $router;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(Client $client, $clientId, $clientSecret, Router $router)
+    public function __construct(ClientRegistry $clientRegistry, EntityManagerInterface $entityManager, RouterInterface $router)
     {
-        $this->client = $client;
-        $this->clientId = $clientId;
-        $this->clientSecret = $clientSecret;
+        $this->clientRegistry = $clientRegistry;
         $this->router = $router;
+        $this->entityManager = $entityManager;
     }
 
-
-    public function createToken(Request $request, $providerKey): PreAuthenticatedToken
+    public function start(Request $request)
     {
-        $code = $request->query->get('code');
+        //return new RedirectResponse($this->router->generate('app_login'));
+    }
 
-        $redirectUri = $this->router->generate('github_redirect_url', [], ROUTER::ABSOLUTE_URL);
-        $url = 'https://github.com/login/oauth/access_token?client_id='.$this->clientId.'&client_secret='.$this->clientSecret.'&code='.$code.'&redirect_uri='.urlencode($redirectUri);
+    public function supports(Request $request): bool
+    {
+        return 'github_redirect_url' === $request->attributes->get('_route') && $request->get('service') === 'github';
+    }
 
+    public function authenticate(Request $request): Passport
+    {
+        $client = $this->clientRegistry->getClient('github');
+        $accessToken = $this->fetchAccessToken($client);
 
-        $response = $this->client->post($url, array(''));
+        return new SelfValidatingPassport(
+            new UserBadge($accessToken->getToken(), function() use ($accessToken, $client, $request) {
 
-        $res = $response->getBody()->getContents();
-        $info = explode('&', $res);
-        $res = explode('=', $info[0]);
+                /** @var GithubResourceOwner $githubUser */
+                $githubUser = $client->fetchUserFromToken($accessToken);
 
-        if (isset($res[0]) && 'error' == $res[0]) {
-            throw new BadCredentialsException('No access_token returned by Github. Start over the process.');
-        }
+                // we get back the email user
+                $response = HttpClient::create()->request(
+                    'GET',
+                    'https://api.github.com/user/emails',
+                    [
+                        'headers' => [
+                            'authorization' => "token {$accessToken}"
+                        ]
+                    ]
+                );
+                $emails = json_decode($response->getContent(), true);
 
-        return new PreAuthenticatedToken(
-            'anon.',
-            $res[1],
-            $providerKey
+                foreach ($emails as $email) {
+                    if ($email['primary'] === true && $email['verified'] === true) {
+                        $data = $githubUser->toArray();
+                        $data['email'] = $email['email'];
+                        $githubUser = new GithubResourceOwner($data);
+                    }
+                }
+                // 1) have they logged in with github before? Easy!
+                $existingUser = $this->entityManager->getRepository(User::class)->findOneBy(['githubId' => $githubUser->getId()]);
+
+                if ($existingUser) {
+                    $request->getSession()->set(Security::LAST_USERNAME, $email['email'] ?? $githubUser->getEmail() ?? 'test@test.fr');
+                    return $existingUser;
+                }
+
+                // 2) do we have a matching user by email?
+                $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+
+                if (!$user) {
+                    $user = new User();
+                    $user->setGithubId($githubUser->getId());
+                    $user->setEmail($githubUser->getEmail() ?? $email['email'] ?? 'test@test.fr');
+                    $user->setFullname($githubUser->getNickname());
+                    $user->setUsername($githubUser->getNickname());
+                    $user->setAvatarUrl($githubUser->getNickname().'_avatar');
+                    $user->setProfileHtmlUrl($githubUser->getNickname().'_profile');
+                    $user->setPassword('test');
+                }
+                $request->getSession()->set(Security::LAST_USERNAME, $user->getEmail());
+                // 3) Maybe you just want to "register" them by creating
+                // a User object
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+                return $user;
+            })
         );
-    }
-
-    public function authenticateToken(TokenInterface $token, UserProviderInterface $userProvider, $providerKey): PreAuthenticatedToken
-    {
-        $accessToken = $token->getCredentials();
-
-        $user = $userProvider->loadUserByUsername($accessToken);
-        return new PreAuthenticatedToken(
-            $user,
-            $accessToken,
-            $providerKey,
-            ['ROLE_USER']
-        );
-    }
-
-    public function supportsToken(TokenInterface $token, $providerKey): bool
-    {
-        return $token instanceof PreAuthenticatedToken && $token->getProviderKey() === $providerKey;
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception) : Response
     {
-        return new Response("Authentication Failed :(", 403);
+        $message = strtr($exception->getMessageKey(), $exception->getMessageData());
+
+        return new Response($message, Response::HTTP_FORBIDDEN);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        // change "app_homepage" to some route in your app
+        $targetUrl = $this->router->generate('diary');
+
+        return new RedirectResponse($targetUrl);
+
+        // or, on success, let the request continue to be handled by the controller
+        //return null;
     }
 }
